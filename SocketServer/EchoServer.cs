d099f08c67f3da16befe385764973834262526f9
skennel2 <skennel2@gmail.com>
2018-04-24 01:05:35 +0900

    first commit

---
+++ SocketServer/EchoServer.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace SocketServer
+{
+    public class EchoServer
+    {
+        public void Start()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 7000);
+            listener.Start();
+
+            byte[] buff = new byte[1024];
+
+            while (true)
+            {
+                using (TcpClient client = listener.AcceptTcpClient())
+                {
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        int byteSize = stream.Read(buff, 0, buff.Length);
+
+                        while (byteSize > 0)
+                        {
+                            stream.Write(buff, 0, byteSize);
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using NLog;
+
+namespace SocketServer
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            logger.Debug("df");
+            new EchoServer().Start();
+        }
+    }
+}
+ 
