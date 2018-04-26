bdac37a6761934121ba22ac5be8cd106b8deda05
skennel2 <skennel2@gmail.com>
2018-04-26 22:31:36 +0900

    missing

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Data;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
+﻿using System.Data;
 
 namespace MultiThreadingPractice
 {
@@ -19,68 +15,6 @@ namespace MultiThreadingPractice
             ByteHandle.PrimitiveTypeToByte();
             ByteHandle.StringTypeToByte();
             ByteHandle.ObjectToByte();
-        }
-    }
-
-    public class ByteHandle
-    {
-        public static void PrimitiveTypeToByte()
-        {
-            Console.WriteLine("PrimitiveTypeToByte");
-            int value = 100;
-
-            byte[] bytes = BitConverter.GetBytes(value);
-
-            foreach(byte b in bytes)
-            {
-                Console.WriteLine(b);
-            }
-        }
-
-        public static void StringTypeToByte()
-        {
-            Console.WriteLine("StringTypeToByte");
-
-            String stringValue = "gggg bbbb";
-
-            byte[] bytesToString = Encoding.Unicode.GetBytes(stringValue);
-
-            foreach (byte b in bytesToString)
-            {
-                Console.WriteLine(b);
-            }
-        }
-
-        public static void ObjectToByte()
-        {
-            Console.WriteLine("ObjectToByte");
-
-            Counter counter = new Counter();
-
-            BinaryFormatter binaryFormatter = new BinaryFormatter();
-            MemoryStream memoryStream = new MemoryStream();
-            binaryFormatter.Serialize(memoryStream, counter);
-
-            byte[] bytes = memoryStream.ToArray();
-
-            foreach (byte b in bytes)
-            {
-                Console.WriteLine(b);
-            }
-
-            ByteToObject(bytes);
-        }
-
-        public static void ByteToObject(byte[] bytes)
-        {
-            Console.WriteLine("ByteToObject");
-            BinaryFormatter binaryFormatter = new BinaryFormatter();
-            MemoryStream memoryStream = new MemoryStream();
-            memoryStream.Write(bytes, 0, bytes.Length);
-            memoryStream.Seek(0, SeekOrigin.Begin); // 뭔지모르겠당 ....
-            object obj = binaryFormatter.Deserialize(memoryStream) as object;
-
-            Console.WriteLine(obj.ToString());
         }
     }
 }
